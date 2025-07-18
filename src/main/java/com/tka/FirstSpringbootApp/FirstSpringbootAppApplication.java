8eb81c45de1460ccaeca65534dbeb1c85e9abbae
rohanchaure1757 <chaurerohan5@gmail.com>
2025-07-18 11:41:02 +0530

    changes in main file

--- src/main/java/com/tka/FirstSpringbootApp/FirstSpringbootAppApplication.java
+++ src/main/java/com/tka/FirstSpringbootApp/FirstSpringbootAppApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FirstSpringbootAppApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(FirstSpringbootAppApplication.class, args);
+		System.out.println("helllo");
 	}
 
 }
